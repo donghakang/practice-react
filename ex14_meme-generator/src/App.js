03b6a46fe3385d72dc65115631d9f91f351e6cfb
Dongha Kang <dkang0602@gmail.com>
2021-03-30 18:08:48 +0900

    meme-generator practice

---
+++ ex14_meme-generator/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import Header from "./components/Header"
+import MemeGenerator from "./components/MemeGenerator"
+
+function App() {
+  return (
+    // <h1>he</h1>
+    <>
+      <Header/>
+      <MemeGenerator/>
+    </>
+  );
+}
+
+export default App;
