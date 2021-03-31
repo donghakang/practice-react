3a4cb61e2229e0521157a3ca5df150d351fba83e
Dongha Kang <dkang0602@gmail.com>
2021-03-31 23:52:47 +0900

    seperate files to component

--- ex12_practice/src/components/ui/Phone.js
+++ ex12_practice/src/components/ui/Phone.js
@@ -1,69 +1,14 @@
-// import Footer from "./components/footer";
 
 
 import * as THREE from "three";
 
-import React, { Suspense, useRef, useState } from "react";
-import "../App.scss";
-import { Canvas, useFrame, useThree } from "react-three-fiber";
+import React, { useRef, useState } from "react";
+import { useFrame } from "react-three-fiber";
 import { useSpring, animated } from "react-spring";
-import { OrbitControls, useGLTF, softShadows, Html } from "@react-three/drei";
-import { Link } from "react-router-dom";
+import { useGLTF, Html } from "@react-three/drei";
 
-import Nav from "./Nav";
+import "../../App.scss";
 
-import Title from "./ui/Title"
-import Statue from "./ui/Statue"
-import Mac from "./ui/Laptop"
-
-let StatueClicked = false;
-let MacClicked = false;
-let PhoneClicked = false;
-
-softShadows();
-const Light = () => {
-  return (
-    <group>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[-5, 2, -10]} intensity={0.5} color="red" />
-      <pointLight position={[10, 3, -5]} intensity={1.5} color="blue" />
-      <pointLight position={[30, -10, -30]} intensity={1.5} color="green" />
-      <directionalLight
-        castShadow
-        position={[0, 40, 40]}
-        intensity={5.0}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={100}
-        shadow-camera-left={-100}
-        shadow-camera-right={100}
-        shadow-camera-top={100}
-        shadow-camera-bottom={-100}
-        shadow-radius={8}
-      />
-    </group>
-  );
-};
-
-const Ground = () => {
-  const { viewport } = useThree();
-  return (
-    <group>
-      <mesh
-        receiveShadow
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, -20, 0]}
-      >
-        <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
-        <meshPhongMaterial attach="material" color="#cccccc" />
-      </mesh>
-    </group>
-  );
-};
-
-/// - Individual objects
-
-/*
 function Phone(props) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/phone.gltf')
@@ -240,53 +185,6 @@ function Phone(props) {
 
   )
 }
-*/
 
-function Objects(props) {
-  const obj = useRef();
-  useFrame(() => {
-    // if (!(PhoneClicked || MacClicked || StatueClicked)) {
-    //   obj.current.rotation.y += 0.005
-    // }
-    // obj.current.rotation.y += 0.005
-  });
 
-  return (
-    <group ref={obj}>
-      <Mac />
-      <Statue />
-      {/* < Phone /> */}
-    </group>
-  );
-}
-
-function Home() {
-  return (
-    <div className="Home">
-      <Nav />
-      <Canvas
-        shadowMap
-        colorManagement
-        camera={{ position: [10, 10, 20], fov: 100 }}
-        style={{
-          width: "70vw",
-          height: "100vh",
-          position: "absolute",
-          top: "0%",
-          left: "30vw",
-        }}
-      >
-        <Light />
-
-        <Suspense fallback={null}>
-          <Objects />
-        </Suspense>
-        {/* <OrbitControls/> */}
-      </Canvas>
-      {/* <Footer /> */}
-      <Title />
-    </div>
-  );
-}
-
-export default Home;
+export default Phone
