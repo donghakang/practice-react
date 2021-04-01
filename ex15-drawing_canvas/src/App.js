4b60e48648af4a4c75d1851313d1c6e56597b653
Dongha Kang <dkang0602@gmail.com>
2021-04-01 23:51:55 +0900

    react canvas

---
+++ ex15-drawing_canvas/src/App.js
@@ -0,0 +1,65 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+function App() {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d");
+    context.scale(2,2)
+    context.lineCap = "round"
+    context.lineJoin = 'round';
+    canvas.style.webkitFilter = "blur(2px)";
+    context.strokeStyle = "rgba(0,0,0.9,1)"
+    context.lineWidth = 5
+    contextRef.current = context;
+    
+  }, [ ])
+
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+
+
+  const draw = ({nativeEvent}) => {
+    if (!isDrawing) return
+
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  }
+
+  return (
+    <>
+    <canvas 
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}/>
+
+    </>
+    
+
+  );
+}
+
+export default App;
