bd9ee8e45b7382eeed7db8dc283c0d02caba8c61
Dongha Kang <dkang0602@gmail.com>
2021-04-26 23:57:37 +0900

    clean up the css

--- ex16-practice2/src/components/About.js
+++ ex16-practice2/src/components/About.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Skill from "./ui/About/Skill";
 import Work from "./ui/About/Work";
 import Resume from "./ui/About/Resume";
+import Education from "./ui/About/Education";
 
 const Info = () => {
   return (
@@ -23,20 +24,22 @@ export default function About() {
           <div className="face"></div>
           <Info />
         </div>
-        <div className="timeline">
-          <div className="time">
+        {/* <div className="timeline">
+          div className="time">
             <div className="line" />
-          </div>
-          <div className="content">
+          </div> 
+          
+        </div> */}
+        <div className="content">
             <div className="content education">
               <h1>Education</h1>
+              <Education />
             </div>
             <div className="content work">
               <h1>Previous Work</h1>
               <Work />
             </div>
           </div>
-        </div>
         <Skill />
         <Resume />
       </div>
--- ex16-practice2/src/components/ui/About/Education.jsx
+++ ex16-practice2/src/components/ui/About/Education.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const EducationElement = (props) => {
-    return (
-        <div>
-            {props.name}
-            {props.description}
-            {props.location}
-            {props.date}
-        </div>
-    );
-}
+  const elementStyle = { backgroundColor: "red" };
+  const titleStyle = { backgroundColor: "yellow" };
+  const dateStyle = { backgroundColor: "green" };
+  const gpaStyle = { backgroundColor: "blue" };
+  const descriptionStyle = { backgroundColor: "yellowgreen" };
+  return (
+    <div style={elementStyle}>
+      <h3 style={titleStyle}>{props.name}</h3>
+      <div style={gpaStyle}>{props.GPA}</div>
+      <div style={descriptionStyle}>{props.description}</div>
+      <div style={dateStyle}>{props.date}</div>
+    </div>
+  );
+};
 
 export default function Education() {
-    return (
-        <div>
-            previous work go here....
-            <EducationElement 
+  return (
+    <div>
+      {/* <EducationElement 
                 name={'Seoul National University Hospital'}
                 description={'Image Processing'}
                 location={'Seoul National University Hospital'}
@@ -38,7 +42,24 @@ export default function Education() {
                 description={'In Gaming'}
                 location={'University of Minnesota'}
                 date={'2021.2 ~ current'}
-            />
-        </div>
-    );
+            /> */}
+
+      <EducationElement
+        name={"Cuyahoga Valley Christian Academy"}
+        GPA={"3.9/4.5"}
+        date={"2011.2 ~ 2014.5"}
+      />
+      <EducationElement
+        name={"University of Minnesota - Twin Cities"}
+        GPA={"3.45/4.0"}
+        description={"Bachelor of Science in Computer Science"}
+        date={"2011.2 ~ 2014.5"}
+      />
+      <EducationElement
+        name={"Certificate for JSP / Android"}
+        description={"국비지원 Java / JSP / Android"}
+        date={"2020.7 ~ 2021.2"}
+      />
+    </div>
+  );
 }
