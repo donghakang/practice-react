a035b818527ff2a3346a5c3044a3b14f1d92a572
Dongha Kang <dkang0602@gmail.com>
2021-05-31 23:06:02 +0900

    📝 Project

--- practice/practice1/src/component/Project.js
+++ practice/practice1/src/component/Project.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import "./ui/Project/Project.scss";
 
-export default function Project() {
+const Name = () => {
+  // const style =
+  const name = "PROJECT";
+  const style = {
+    backgroundColor: "white",
+    fontSize: "6rem",
+    color: "darkblue",
+    zIndex: "-1",
+  };
+
+  return <h1 style={style}>{name}</h1>;
+};
+
+
+const ProjectComponent = () => {
     return (
-        <div>
-            <h1>
-                Project page coming soon.
-            </h1>
+        <div className="component">
+            Project
         </div>
     )
+}
+
+const Projects = () => {
+    return (
+        <div className="contents">
+            <ProjectComponent/>
+            <ProjectComponent/>
+            <ProjectComponent/>
+            <ProjectComponent/>
+            <ProjectComponent/>
+            <ProjectComponent/>
+            
+            <ProjectComponent/>
+            <ProjectComponent/>
+            <ProjectComponent/>
+            <ProjectComponent/>
+        </div>
+    )
+}
+
+
+export default function Project() {
+  return (
+    <div className="project">
+      <Name />
+      <Projects/>
+    </div>
+  );
 }
