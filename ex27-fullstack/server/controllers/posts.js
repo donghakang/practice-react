643492db177351a8958c470aa6cf49e3384ea334
Dongha Kang <dkang0602@gmail.com>
2021-09-04 22:30:04 +0900

     🏻‍🔬 Fullstack MERN

---
+++ ex27-fullstack/server/controllers/posts.js
@@ -0,0 +1,27 @@
+// all the handler goes here
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  //   res.send("This works");
+  try {
+    const postMessages = await PostMessage.find(); // async
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const createPosts = async (req, res) => {
+  //   res.send("Post Creation");
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
